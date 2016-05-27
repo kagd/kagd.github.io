be01576707c6c03784f7b1a69b934f63514864ab
Grant Klinsing <grant@digitalopera.com>
2016-05-26 23:40:28 -0600

    Automated commit at 2016-05-27 05:40:28 UTC by middleman-deploy 1.0.0

--- javascripts/application.js
+++ javascripts/application.js
@@ -359,3 +359,21 @@
   angular.module('kagd').factory('githubFactory', Factory);
 
 }).call(this);
+(function() {
+  var Controller;
+
+  Controller = function() {
+    var ctrl;
+    ctrl = this;
+    ctrl.open = false;
+    ctrl.toggle = function() {
+      return ctrl.open = !ctrl.open;
+    };
+  };
+
+  angular.module('kagd').component('kagdTagsActionsheet', {
+    templateUrl: '/templates/tags_actionsheet/tags_actionsheet_component.html',
+    controller: Controller
+  });
+
+}).call(this);
